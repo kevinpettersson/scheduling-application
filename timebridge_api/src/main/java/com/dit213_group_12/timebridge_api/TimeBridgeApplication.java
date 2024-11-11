2c379032c9d28894f00277e9beb11ad344148fa9
Sebastian Pålsson <sebastian@mac.local>
2024-11-11 11:05:16 +0100

    create springboot api application with maven

---
+++ timebridge_api/src/main/java/com/dit213_group_12/timebridge_api/TimeBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.dit213_group_12.timebridge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeBridgeApplication.class, args);
+	}
+
+}
---
+++ timebridge_api/src/test/java/com/dit213_group_12/timebridge_api/TimebridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dit213_group_12.timebridge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimebridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
