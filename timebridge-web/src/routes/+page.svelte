afaabdc7c1fb383de4d52a8222bbb7eafecc795a
lyrevik <lyrevik@student.chalmers.se>
2024-11-13 10:08:32 +0100

    en basic början i Svelte

--- timebridge-web/src/routes/+page.svelte
+++ timebridge-web/src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script> 
+    let messege = "Webbapp för OOP"   
+</script>    
+
+<style>
+    body {
+        font-family: Arial, Sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #f9f9f9;
+        color: black;
+    }    
+    header {
+        background-color: #6200ea;
+        color: white;
+        padding: 1rem;
+        text-align: center;
+    }
+
+    main {
+        padding: 2rem;
+        text-align: center;
+    }
+
+    footer {
+        background-color: #333;
+        color: white;
+        padding: 1rem;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+    }
+</style>
+
+<header>
+    <h1>
+        Scheduling app website!
+    </h1>
+</header>
+
+<main>  
+    <h2>
+        {messege}
+    </h2>
+    <p>
+        text hahahahah text hahahahah
+    </p>
+    <!-- INTERACTIVE COMPONENTS HERE -->
+     <button on:click={() => alert('ButtonHasBeen clicked but does it do anythin?!')}>CLICK ME!!!!!</button>
+    
+</main>
+
+<footer>
+    <p>
+        &copy; 2024 Object Oriented Project Course
+    </p>
+    <p>
+        &copy; group 12
+    </p>
+    <p>
+        Kevin, Natalie, Nils, Oscar, Sebastian
+    </p>
+</footer>
+
+
