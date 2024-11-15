0a9c0f76407cb139ff4d666b247fadb6d926e670
Sebastian Pålsson <sebastian@mac.local>
2024-11-15 11:59:46 +0100

    removed tests hehe :)

--- timebridge/src/main/java/timebridge/Controller.java
+++ timebridge/src/main/java/timebridge/Controller.java
@@ -1,19 +1,13 @@
 package timebridge;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import timebridge.model.*;
