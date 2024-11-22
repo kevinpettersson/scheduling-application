578b6b688c2b586f3c080df9e4c643856aebda71
Kevin Pettersson <kevin.petterssons@hotmail.com>
2024-11-22 14:24:47 +0100

    pulled latest changes from main branch, continuing running tests on settings.

--- timebridge/src/test/java/timebridge/CalendarParserTests.java
+++ timebridge/src/test/java/timebridge/CalendarParserTests.java
@@ -1,4 +1,5 @@
 package timebridge;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -8,6 +9,9 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,14 +20,19 @@ import org.springframework.util.StringUtils;
 import timebridge.model.Calendar;
 import timebridge.model.CalendarBuilder;
 import timebridge.model.CalendarParser;
+import timebridge.model.Event;
 
 @SpringBootTest
 public class CalendarParserTests {
+
+    public String getTextFile(String file) throws IOException, URISyntaxException{
+        // Retrive the ics-file from the given path and turn it into a String.
+        return new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("testfiles/"+ file +".ics").toURI())));
+    }
     
     @Test
     public void testParseEmptyFile() throws IOException, URISyntaxException{
-        // Retrive the ics-file from the given path and turn it into a String.
-        String ics = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("testfiles/empty_file.ics").toURI())));
+        String ics = getTextFile("empty_file");
 
         // Build the Calendar object from the String using CalendarBuilder. 
         Calendar calendar = CalendarBuilder.build(ics);
@@ -55,8 +64,7 @@ public class CalendarParserTests {
 
     @Test 
     public void testParseCalendarWithNoEvents() throws IOException, URISyntaxException{
-
-        String ics = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("testfiles/no_events.ics").toURI())));
+        String ics = getTextFile("no_events");
 
         Calendar calendar = CalendarBuilder.build(ics);
 
@@ -73,25 +81,25 @@ public class CalendarParserTests {
 
     @Test
     public void testParseOneEvent() throws IOException, URISyntaxException{
-        String ics = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("testfiles/one_event.ics").toURI())));
+        String ics = getTextFile("one_event");
 
-       Calendar calendar = CalendarBuilder.build(ics);
+        Calendar calendar = CalendarBuilder.build(ics);
        
-       String iCalContent = CalendarParser.parse(calendar);
+        String iCalContent = CalendarParser.parse(calendar);
 
-       assertNotNull(iCalContent);
-       assertTrue(iCalContent.contains("BEGIN:VCALENDAR"));
-       assertTrue(iCalContent.contains("END:VCALENDAR"));
-        
-       // Verify that the file only contains 2 events if there only are 2 occurences of Kurskod:
-       assertEquals(2, StringUtils.countOccurrencesOf(iCalContent, "BEGIN:VEVENT"));
-       assertEquals(2, StringUtils.countOccurrencesOf(iCalContent,"END:VEVENT"));
+        assertNotNull(iCalContent);
+        assertTrue(iCalContent.contains("BEGIN:VCALENDAR"));
+        assertTrue(iCalContent.contains("END:VCALENDAR"));
+            
+        // Verify that the file only contains 2 events if there only are 2 occurences of Kurskod:
+        assertEquals(2, StringUtils.countOccurrencesOf(iCalContent, "BEGIN:VEVENT"));
+        assertEquals(2, StringUtils.countOccurrencesOf(iCalContent,"END:VEVENT"));
     }
 
     @Test
     public void testParseTwoEvents() throws IOException, URISyntaxException{
         
-       String ics = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("testfiles/two_events.ics").toURI())));
+       String ics = getTextFile("two_events");
 
        Calendar calendar = CalendarBuilder.build(ics);
        
@@ -107,7 +115,35 @@ public class CalendarParserTests {
     }
 
     @Test
-    public void testTimeCorrectTimeFormat(){
+    public void testTimeCorrectTimeFormat() throws IOException, URISyntaxException{
         
+        String ics = getTextFile("two_courses");
+
+        Calendar calendar = CalendarBuilder.build(ics);
+        
+        String iCalContent = CalendarParser.parse(calendar);
+
+        assertNotNull(iCalContent);
+        assertTrue(iCalContent.contains("BEGIN:VCALENDAR"));
+        assertTrue(iCalContent.contains("END:VCALENDAR"));
+
+        // defines the expected date-format for verification. 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss'Z'").withZone(ZoneOffset.UTC);
+
+        for (Event event : calendar.getEvents()) {
+            
+            ZonedDateTime start = event.getInterval().getStart();
+            ZonedDateTime end = event.getInterval().getEnd();
+
+            // verify start comes before the end.
+            assertTrue(start.isBefore(end));
+
+            // Format the ZonedDateTime objects into strings
+            String formattedStart = formatter.format(start);
+            String formattedEnd = formatter.format(end);
+
+            // verify the string matches the expected pattern: "yyyyMMdd'T'HHmmss'Z'"
+            assertTrue(formattedStart.matches("\\d{8}T\\d{6}Z"));
+        }
     }
 }
